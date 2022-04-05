987f377eecf85d4839fe84595596ba7d10745db0
Christopher Yamas <yamas.chris@gmail.com>
2022-04-04 19:30:26 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,62 +1,31 @@
 import React from 'react';
-import axios from 'axios';
-import NotesDisplay from './NotesDisplay';
-import NotesForm from './NotesForm';
-import './App.css'; 
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+import './App.css';
 
-const API_SERVER = process.env.REACT_APP_API;
 
 class App extends React.Component {
-
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      notes: []
+      email: null,
+      location: "seattle"
     }
-  }
-
-  addNote = async (note) => {
-    await axios.post(`${API_SERVER}/notes`, note);
-    this.getNotes();
-  }
-
-  getNotes = async () => {
-    const response = await axios.get(`${API_SERVER}/notes`);
-    const notes = response.data;
-    this.setState({ notes });
-  }
-
-  deleteNote = async (id) => {
-    try {
-      let url = `${API_SERVER}/notes/${id}`
-      await axios.delete(url);
-      let updatedNotes = this.state.notes.filter(note => note._id !== id);
-      this.setState({
-        notes: updatedNotes
-      });
-    } catch(error) {
-      console.log('There is an error')
-    }
-  }
-
-  async componentDidMount() {
-    await this.getNotes();
   }
 
   render() {
     return (
       <>
-        <h1>Your Notes</h1>
-        <NotesForm
-          handleAddNote={this.addNote}
-        />
-        <NotesDisplay
-          notesList={this.state.notes}
-          deleteNote={this.deleteNote}
-        />
+
+        <Header />
+        <Main />
+        <Footer />
+
       </>
-    );
+    )
   }
 }
+
 
 export default App;
