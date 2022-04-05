036b171ae5084c18c364938de061feaf078970ca
Christopher Yamas <yamas.chris@gmail.com>
2022-04-04 19:18:00 -0500

    note add get delete functions add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import NotesDisplay from './NotesDisplay';
+import NotesForm from './NotesForm';
+import './App.css'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_SERVER = process.env.REACT_APP_API;
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: []
+    }
+  }
+
+  addNote = async (note) => {
+    await axios.post(`${API_SERVER}/notes`, note);
+    this.getNotes();
+  }
+
+  getNotes = async () => {
+    const response = await axios.get(`${API_SERVER}/notes`);
+    const notes = response.data;
+    this.setState({ notes });
+  }
+
+  deleteNote = async (id) => {
+    try {
+      let url = `${API_SERVER}/notes/${id}`
+      await axios.delete(url);
+      let updatedNotes = this.state.notes.filter(note => note._id !== id);
+      this.setState({
+        notes: updatedNotes
+      });
+    } catch(error) {
+      console.log('There is an error')
+    }
+  }
+
+  async componentDidMount() {
+    await this.getNotes();
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Your Notes</h1>
+        <NotesForm
+          handleAddNote={this.addNote}
+        />
+        <NotesDisplay
+          notesList={this.state.notes}
+          deleteNote={this.deleteNote}
+        />
+      </>
+    );
+  }
 }
 
 export default App;
