5ad986d4d4a497353963e091b02a9701e415545b
Christopher Yamas <yamas.chris@gmail.com>
2022-04-04 20:59:40 -0500

    notes components add

--- src/App.js
+++ src/App.js
@@ -4,14 +4,44 @@ import Main from './Main';
 import Footer from './Footer';
 import './App.css';
 
+const API_SERVER = process.env.REACT_APP_SERVER;
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       email: null,
-      location: "seattle"
+      location: "seattle",
+      notes: []
     }
+  }
+
+  addNote = async (note) => {
+    await axios.post(`${API_SERVER}/notes`, note);
+    this.getNotes();
+  }
+
+  getNotes = async () => {
+    const response = await axios.get(`${API_SERVER}/notes`);
+    const notes = response.data;
+    this.setState({ notes });
+  }
+
+  deleteNote = async (id) => {
+    try {
+      let url = `${API_SERVER}/notes/${id}`
+      await axios.delete(url);
+      let updatedNotes = this.state.notes.filter(note => note._id !== id);
+      this.setState({
+        notes: updatedNotes
+      });
+    } catch(error) {
+      console.log('There is an error')
+    }
+  }
+
+  async componentDidMount() {
+    await this.getNotes();
   }
 
   render() {
@@ -19,6 +49,11 @@ class App extends React.Component {
       <>
 
         <Header />
+        <NotesForm
+          handleAddNote={this.addNote}/>
+        <NotesDisplay
+          notesList={this.state.notes}
+          deleteNote={this.deleteNote}/>
         <Main />
         <Footer />
 
