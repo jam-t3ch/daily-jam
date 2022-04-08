01883f654cb229536419bd03b172f15e34856f6d
James Brooks <brooksjamesk01@gmail.com>
2022-04-07 17:38:46 -0700

    added NewsCarousel component

---
+++ src/NewsCarousel.js
@@ -0,0 +1,38 @@
+import React from 'react';
+// import { useState } from 'react';
+import { Container, Carousel } from 'react-bootstrap';
+import './NewsCarousel.css';
+
+const NewsCarousel = (props) => {
+  // console.log('props.news on carousel:', props)
+  return (
+    <>
+      {
+
+        props.news
+
+          ?
+          <Container style={{ width: '100%', height: 'max-content' }}>
+            <Carousel controls={false}>
+              {props.news.map((story, i) => (
+
+                <Carousel.Item
+                  key={i}
+                  className="news-carousel"
+                >
+                  <p>{story.title}</p>
+                  <p>{story.description}</p>
+                </Carousel.Item>
+              ))}
+            </Carousel>
+          </Container>
+
+          :
+          null
+
+      }
+    </>
+  )
+}
+
+export default NewsCarousel
